cbbf2c74525bdb7e604db380a61fe3bbad9672fa
Testowyinz <testowyinz@gmail.com>
2018-10-07 15:55:24 +0200

    Wyświetlenie kalendarza i jego obsługa

--- src/EventListener/FullCalendarListener.php
+++ src/EventListener/FullCalendarListener.php
@@ -2,10 +2,11 @@
 
 namespace App\EventListener;
 
-//use App\Entity\Events;
+use App\Entity\Booking;
 use Doctrine\ORM\EntityManagerInterface;
 use Toiba\FullCalendarBundle\Entity\Event;
 use Toiba\FullCalendarBundle\Event\CalendarEvent;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class FullCalendarListener
 {
@@ -13,10 +14,15 @@ class FullCalendarListener
      * @var EntityManagerInterface
      */
     private $em;
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $router;
 
-    public function __construct(EntityManagerInterface $em )
+    public function __construct(EntityManagerInterface $em, UrlGeneratorInterface $router)
     {
         $this->em = $em;
+        $this->router = $router;
     }
 
     public function loadEvents(CalendarEvent $calendar)
@@ -52,6 +58,11 @@ class FullCalendarListener
             // $bookingEvent->setUrl('http://www.google.com');
             // $bookingEvent->setBackgroundColor($booking->getColor());
             // $bookingEvent->setCustomField('borderColor', $booking->getColor());
+            $bookingEvent->setUrl(
+                $this->router->generate('booking_show', array(
+                    'id' => $booking->getId(),
+                ))
+            );
 
             // finally, add the booking to the CalendarEvent for displaying on the calendar
             $calendar->addEvent($bookingEvent);
